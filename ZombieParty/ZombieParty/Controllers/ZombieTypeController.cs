d23a4a2e4b036eb3fa5c995daf8678bf88f6a245
Ryan Bob <2239948@cegepmontpetit.ca>
2026-09-17 14:09:55 -0400

    FCT ZombieType Edit et ZombieType Delete
    Ajout de la View Edit, Delete et de leurs actions dans ZombieTypeController

--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ZombieParty.Models;
 using ZombieParty.Models.Data;
 using ZombieParty.ViewModels;
@@ -58,6 +59,57 @@ namespace ZombieParty.Controllers
             }
 
             return this.View(zombieType);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ZombieTypeVM zombieTypeVM = new ZombieTypeVM();
+            zombieTypeVM.ZombieType = _baseDonnees.ZombieTypes.Find(id);
+
+            return View(zombieTypeVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ZombieTypeVM zombieTypeVM)
+        {
+            //Si le modèle est valide le zombieType est modifié et nous sommes redirigé vers index.
+            if (ModelState.IsValid)
+            {
+                _baseDonnees.ZombieTypes.Update(zombieTypeVM.ZombieType);
+                _baseDonnees.SaveChanges();
+                TempData["Success"] = $"ZombieType {zombieTypeVM.ZombieType.TypeName} has been modified";
+                return this.RedirectToAction("Index");
+            }
+
+            return View(zombieTypeVM);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            ZombieTypeVM zombieTypeVM = new ZombieTypeVM();
+            zombieTypeVM.ZombieType = _baseDonnees.ZombieTypes.Find(id);
+
+            if (zombieTypeVM == null)
+            {
+                return NotFound();
+            }
+
+            return View(zombieTypeVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id)
+        {
+            ZombieType? zombieTypeVM = _baseDonnees.ZombieTypes.Find(id);
+            if (zombieTypeVM == null)
+            {
+                return NotFound();
+            }
+
+            _baseDonnees.ZombieTypes.Remove(zombieTypeVM);
+            _baseDonnees.SaveChanges();
+            TempData["Success"] = $"ZombieType {zombieTypeVM.TypeName} terminated";
+            return RedirectToAction("Index");
         }
 
     }
