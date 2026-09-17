590bf2fc1f949b8d1804ce125568d6805e2ab102
Ryan Bob <2239948@cegepmontpetit.ca>
2026-09-17 12:11:36 -0400

    FCT Seed Zombie ZombieType et Weapon
    Création données de départ pour Zombie, ZombieType et Weapon

--- ZombieParty/ZombieParty/Migrations/20260917160112_SeedZombieZombieTypeWeapon.Designer.cs
+++ ZombieParty/ZombieParty/Migrations/20260917160112_SeedZombieZombieTypeWeapon.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZombieParty.Models.Data;
 
@@ -11,9 +12,11 @@ using ZombieParty.Models.Data;
 namespace ZombieParty.Migrations
 {
     [DbContext(typeof(ZombiePartyDbContext))]
-    partial class ZombiePartyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260917160112_SeedZombieZombieTypeWeapon")]
+    partial class SeedZombieZombieTypeWeapon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -120,18 +123,6 @@ namespace ZombieParty.Migrations
                             Force = 10m,
                             Image = "https://publicdomainvectors.org/photos/Shield-ClassicMedieval1.png",
                             Name = "Master Shield",
-                            Price = 500m,
-                            Qty = 30,
-                            QtyBought = 1
-                        },
-                        new
-                        {
-                            WeaponId = 3,
-                            CreatedDate = new DateTime(1999, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "An awesome Stainless Steel Sword",
-                            Force = 70m,
-                            Image = "https://cdn.wikimg.net/en/zeldawiki/images/thumb/c/c5/TotK_Master_Sword_Model.png/800px-TotK_Master_Sword_Model.png",
-                            Name = "Master Sword",
                             Price = 500m,
                             Qty = 30,
                             QtyBought = 1
---
+++ ZombieParty/ZombieParty/Migrations/20260917160900_SeedMonWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZombieParty.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMonWeapon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Weapons",
+                columns: new[] { "WeaponId", "CreatedDate", "Description", "Force", "Image", "Name", "Price", "Qty", "QtyBought" },
+                values: new object[] { 3, new DateTime(1999, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "An awesome Stainless Steel Sword", 70m, "https://cdn.wikimg.net/en/zeldawiki/images/thumb/c/c5/TotK_Master_Sword_Model.png/800px-TotK_Master_Sword_Model.png", "Master Sword", 500m, 30, 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Weapons",
+                keyColumn: "WeaponId",
+                keyValue: 3);
+        }
+    }
+}
